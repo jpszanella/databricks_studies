fa50fdce8d89a0f60e06f3181416e38587c63d15
João Pedro S Zanella <123907394+jpszanella@users.noreply.github.com>
2023-08-07 13:17:40 -0300

    Update hive_partitions.sql

--- hive_partitions.sql
+++ hive_partitions.sql
@@ -4,4 +4,12 @@
   - Dinâmico: só fornecer a coluna referência
   SET hive.exec.dynamic.partition = true; -- habilitar o uso das partições dinâmicas
   SET hive.exec.dynamic.partition.mode = nonstrict; -- permitir a criação de partições baseadas em mais de uma coluna
-  - 
+2. Apache Spark:
+spark.sql('query').show()
+ou 
+display(spark.sql('query'))
+  - Se quisermos pular linhas na query temos que utilizar 3 aspas simples:
+spark.sql( '''
+  query
+''' ).show()
+
